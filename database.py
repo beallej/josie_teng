d066210cd6a1cf6b6b436ac9f12b4b424ae1dd09
yuuteng <yuuteng@foxmail.com>
2018-10-18 17:37:32 +0200

    add mission

--- database.py
+++ database.py
@@ -3,23 +3,20 @@ from sqlalchemy.orm import relationship
 from webapp import app
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 
 class Mission(db.Model):
     __tablename__ = 'mission'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.Text)
     description = db.Column(db.Text)
-    categories = db.Column(db.ARRAY)
+    category = db.Column(db.Text)
     date_saisie = db.Column(db.DateTime)
     date_closed = db.Column(db.DateTime, nullable=True)
     status = db.Column(db.Text)
     ingenieurs_positionnes = relationship("Positionnement", back_populates="mission")
     ingenieur_affectue = relationship("Affectuation", back_populates="mission")
-
-
-
 
 class Ingenieur_Etudes(db.Model):
     __tablename__ = 'ingenieur_etudes'
@@ -28,15 +25,13 @@ class Ingenieur_Etudes(db.Model):
     missions_positiones = relationship("Positionnement", back_populates="ingenieur_etudes")
     missions_affectues = relationship("Affectuation", back_populates="ingenieur_etudes")
 
-
-
 class Positionnement(db.Model):
     __tablename__ = 'positionnement'
     voeux = db.Column(db.Text)
     mission_id = db.Column(db.Integer, db.ForeignKey('mission.id'), primary_key=True)
     ingenieur_etudes_id = db.Column(db.Integer, db.ForeignKey('ingenieur_etudes.id'),  primary_key=True)
-    date_positione = date_saisie = db.Column(db.DateTime)
-
+    date_positione = db.Column(db.DateTime)
+    date_saisie = db.Column(db.DateTime)
     ingenieur_etudes = relationship("Ingenieur_Etudes", back_populates="missions_positiones")
     mission = relationship("Mission", back_populates="ingenieurs_positionnes")
 
--- webapp.py
+++ webapp.py
@@ -1,10 +1,43 @@
-import flask
+import datetime
 
-app = flask.Flask(__name__)
+from flask import Flask, request, render_template
 
-@app.route("/")
+app = Flask(__name__)
+app.secret_key = 'ytkey'
+
+@app.route("/",methods=['GET','POST'])
 def index():
-    return flask.render_template("index.html")
+    if request.method == 'POST':
+        # get data from html
+        title = request.form["title"]
+        description = request.form["description"]
+        category = request.form["category"]
+        date_saisie = datetime.date.today()
+
+        if title == "" or description == "":
+            print("No title or No description")
+
+        # add mission in database
+        from database import db, Mission
+        new_mission = Mission()
+        new_mission.title = title
+        new_mission.description = description
+        new_mission.category = category
+        new_mission.date_saisie = date_saisie
+        new_mission.status = ""
+        # ingenieurs_positionnes
+        # ingenieur_affectue
+        db.session.add(new_mission)
+        db.session.commit()
+        print("add successfully")
+
+    return render_template("index.html")
 
 if __name__ == "__main__":
+    # Create the DB
+    from database import db
+    print("creating database")
+    db.create_all()
+    print("database created")
+
     app.run()
