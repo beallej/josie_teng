7710d4d1592b722176037867fdbea0bdb48c8620
yuuteng <yuuteng@foxmail.com>
2018-11-06 17:49:29 +0100

    [yuteng]|fix some bug

--- init.py
+++ init.py
@@ -1,12 +1,15 @@
 from flask import Flask, request, render_template, flash, session
 
+from service import *
+
 app = Flask(__name__)
 app.secret_key = 'ytkey'
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
+
 # Login
-@app.route('/login',methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         # get data from html
@@ -15,15 +18,17 @@ def index():
         if username == "" or password == "":
             wrongMsg = True
             print("No username or No password")
-            return render_template('login.html',wrongMsg=wrongMsg)
+            return render_template('login.html', wrongMsg=wrongMsg)
         else:
             from service import login
-            ingenieur = login(username,password)
-            if ingenieur != None :
+            ingenieur = login(username, password)
+            if ingenieur != None:
                 print("login success")
-                from database import db, Mission
-                missions = Mission.query.all()
-                return render_template('showmissions.html',ingenieur=ingenieur,missions=missions)
+                return showMissions()
+
+                # from database import db, Mission
+                # missions = Mission.query.all()
+                # return render_template('showmissions.html', ingenieur=ingenieur, missions=missions)
 
             else:
                 # flash("login defeat")
@@ -31,8 +36,9 @@ def index():
                 # return render_template('login.html')
     return render_template('login.html')
 
+
 # Register
-@app.route('/register',methods=['GET','POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         # get data from html
@@ -45,18 +51,19 @@ def register():
             print("No username or No password or No confirmpassword or name")
         elif password == password2:
             # if existe? same username
-            if type == "Etude" :
+            if type == "Etude":
                 from service import create_account_pour_ingenieur_etudes
-                create_account_pour_ingenieur_etudes(username,password,name)
+                create_account_pour_ingenieur_etudes(username, password, name)
                 print("create account etude successful")
-            else: # "Affaire"
+            else:  # "Affaire"
                 from service import create_account_pour_ingenieur_affaires
-                create_account_pour_ingenieur_affaires(username,password,name)
+                create_account_pour_ingenieur_affaires(username, password, name)
                 print("create account affaire successful")
     return render_template('Register.html')
 
+
 # add Mission
-@app.route('/addmission',methods=['GET','POST'])
+@app.route('/addmission', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
         # get data from html
@@ -69,21 +76,27 @@ def add():
             print("No title or No description")
         # add mission in database
         from service import addMission
-        addMission(title, description,categories)
+        addMission(title, description, categories)
         print("add successfully")
 
     return render_template('addmission.html')
 
+
 # show Missions
-@app.route('/showmissions',methods=['GET','POST'])
+@app.route('/showmissions', methods=['GET', 'POST'])
 def showMissions():
-    from database import db, Mission
-    missions = Mission.query.all()
-    return render_template('showmissions.html',missions=missions)
+    missionsAAffecter = getMissionsAAffecter()
+    missionsAffectes = getMissionsAffectes()
+    print(missionsAAffecter)
+    missionsClosed = getMissionsClosed()
+    return render_template('showmissions.html', missionsAAffecter=missionsAAffecter, missionsAffectes=missionsAffectes,
+                           missionsClosed=missionsClosed)
+
 
 @app.errorhandler(404)
 def not_found(e):
     return render_template('404.html')
+
 
 if __name__ == "__main__":
     # from database import db
